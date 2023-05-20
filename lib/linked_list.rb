d4e6c7a49a17b51017647b075da652332128a956
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-05-20 17:39:06 +0200

    feat: add initialize method to LinkedList

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
-class LinkedList; end
+class LinkedList
+
+  attr_accessor :size, :head, :tail
+
+  def initialize
+    @head = nil
+    @tail = nil
+    @size = 0
+  end
+end
 
 class Node
   attr_accessor :value, :next_node
